5587517cc2819b5df368e7b2698f14ca72156e50
ZhanSUV <zsuenbaev@gmail.com>
2017-02-16 20:48:14 +0500

    Alpha v 1.0

--- Лаба 1(треугольник)/Program.cs
+++ Лаба 1(треугольник)/Program.cs
@@ -10,95 +10,38 @@ namespace Лаба_1_треугольник_
     {
         public int X;
         public int Y;
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
     }
     class Edge
     {
-        public Point start = new Point();
-        public Point end = new Point();
+        public Point start;
+        public Point end;
+        public Edge(Point start, Point end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+        public double Side()
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(end.X - start.X, 2) + Math.Pow(end.Y - start.Y, 2)), 0);
+        }
     }
     class Triangle
     {
-        public Edge ab = new Edge();
-        public Edge bc = new Edge();
-        public Edge ac = new Edge();
-    }
-    class Program
-    {
-        static void Main(string[] args)
+        public Point[] points;
+        public Edge[] edges;
+        public Triangle(Point[] points, Edge[] edges)
         {
-            Random Gen = new Random();
-            double sumOfRight = 0;
-            double sumOfIsosceles = 0;
-            Triangle[] triangles = new Triangle[10];
-            int count = triangles.Length;
-            for (int i = 0; i < triangles.Length; i++)
-            {
-                triangles[i] = new Triangle();
-                triangles[i].ab.start.X = Gen.Next(1, 3);
-                triangles[i].ab.end.X = Gen.Next(4, 10);
-                triangles[i].ab.start.Y = Gen.Next(1, 3);
-                triangles[i].ab.end.Y = Gen.Next(4, 10);
-                double ab = Side(triangles[i].ab.start.X, triangles[i].ab.end.X, triangles[i].ab.start.Y, triangles[i].ab.end.Y);
-                triangles[i].bc.start.X = triangles[i].ab.end.X;
-                triangles[i].bc.end.X = Gen.Next(4, 6);
-                triangles[i].bc.start.Y = triangles[i].ab.end.Y;
-                triangles[i].bc.end.Y = Gen.Next(5, 10);
-                double bc = Side(triangles[i].bc.start.X, triangles[i].bc.end.X, triangles[i].bc.start.Y, triangles[i].bc.end.Y);
-                triangles[i].ac.start.X = triangles[i].ab.start.X;
-                triangles[i].ac.end.X = triangles[i].bc.end.X;
-                triangles[i].ac.start.Y = triangles[i].ab.start.Y;
-                triangles[i].ac.end.Y = triangles[i].bc.end.Y;
-                double ac = Side(triangles[i].ac.start.X, triangles[i].ac.end.X, triangles[i].ac.start.Y, triangles[i].ac.end.Y);
-                Console.WriteLine("Triangle {0} with sides: AB = {1}, BC = {2}, AC = {3}", i + 1, ab, bc, ac);
-                double perimeter = Perimeter(ab, bc, ac);
-                double area = Area(ab, bc, ac);
-                bool isoscelesTriangle = Isosceles(ab, bc, ac);
-                bool rightTriangle = Right(ab, bc, ac);
-                if (isoscelesTriangle == true)
-                {
-                    sumOfIsosceles += area;
-                }
-                if (rightTriangle == true)
-                {
-                    sumOfRight += perimeter;
-                }
-                Console.WriteLine();
-            }
-            double avgSumOfPerimeters = sumOfRight / count;
-            double avgSumOfArea = sumOfIsosceles / count;
-            Console.WriteLine("Avg sum of right triangle's perimeters = {0}", avgSumOfPerimeters);
-            Console.WriteLine("Avg sum of isoscele triangle's area = {0}", avgSumOfArea);
-            Console.ReadLine();
+            this.points = points;
+            this.edges = edges;
         }
-        public static double Side(int x1, int x2, int y1, int y2)
+        public bool Isosceles()
         {
-            double side = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)), 0);
-            return side;
-        }
-        public static double Perimeter(double ab, double bc, double ac)
-        {
-            double perimeter = ab + bc + ac;
-            Console.WriteLine("Perimeter of triangle = " + perimeter);
-            return perimeter;
-        }
-        public static double Area(double ab, double bc, double ac)
-        {
-            double p = (ab + bc + ac) / 2;
-            double area = Math.Round(Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac)), 2);
-            if (area == null)
-            {
-                Console.WriteLine("Area is doesn't exist or it has negative result");
-                return 0;
-            }
-            else
-            {
-                Console.WriteLine("Area of triangle = " + area);
-                return area;
-            }
-        }
-        public static bool Isosceles(double ab, double bc, double ac)
-        {
-            if (ab == bc || bc == ac || ab == ac)
+            if (edges[0].Side() == edges[1].Side() || edges[1].Side() == edges[2].Side() || edges[0].Side() == edges[2].Side())
             {
                 Console.WriteLine("This triangle is isosceles");
                 return true;
@@ -106,22 +49,22 @@ namespace Лаба_1_треугольник_
             else
             {
                 Console.WriteLine("This triangle is not isosceles");
-                return false;
+                return true;
             }
         }
-        public static bool Right(double ab, double bc, double ac)
+        public bool Right()
         {
-            if (Math.Pow(ab, 2) + Math.Pow(bc, 2) == Math.Pow(ac, 2))
+            if (Math.Pow(edges[0].Side(), 2) + Math.Pow(edges[1].Side(), 2) == Math.Pow(edges[2].Side(), 2))
             {
                 Console.WriteLine("This triangle is right");
                 return true;
             }
-            else if (Math.Pow(ab, 2) + Math.Pow(ac, 2) == Math.Pow(bc, 2))
+            else if (Math.Pow(edges[1].Side(), 2) + Math.Pow(edges[2].Side(), 2) == Math.Pow(edges[0].Side(), 2))
             {
                 Console.WriteLine("This triangle is right");
                 return true;
             }
-            else if (Math.Pow(ac, 2) + Math.Pow(bc, 2) == Math.Pow(ab, 2))
+            else if (Math.Pow(edges[2].Side(), 2) + Math.Pow(edges[0].Side(), 2) == Math.Pow(edges[1].Side(), 2))
             {
                 Console.WriteLine("This triangle is right");
                 return true;
@@ -132,6 +75,93 @@ namespace Лаба_1_треугольник_
                 return false;
             }
         }
+        public double Perimeter()
+        {
+            double perimeter = edges[0].Side() + edges[1].Side() + edges[2].Side();
+            Console.WriteLine("Perimeter of triangle = " + perimeter);
+            return perimeter;
+        }
 
+        public double Area()
+        {
+            double p = (edges[0].Side() + edges[1].Side() + edges[2].Side()) / 2;
+            double area = Math.Round(Math.Sqrt(p * (p - edges[0].Side()) * (p - edges[1].Side()) * (p - edges[2].Side())), 2);
+            Console.WriteLine("Area of triangle = " + area);
+            return area;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Gen = new Random();
+            Point[] points = new Point[3];
+            Edge[] edges = new Edge[3];
+            double sumOfRight = 0;
+            double sumOfIsosceles = 0;
+            Triangle[] triangles = new Triangle[10];
+            int count = triangles.Length;
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                for (int j = 0; j < points.Length; j++)
+                {
+                    points[j] = new Point(Gen.Next(1, 5), Gen.Next(1, 5));
+                }
+                if (points[0] == points[1])
+                {
+                    points[0].X++;
+                    points[0].Y++;
+                    if (points[0] == points[2])
+                    {
+                        points[0].X++;
+                        points[0].Y++;
+                    }
+                }
+                if (points[1] == points[2])
+                {
+                    points[2].X++;
+                    points[2].Y++;
+                    if (points[2] == points[0])
+                    {
+                        points[2].X++;
+                        points[2].Y++;
+                    }
+                }
+                for (int k = 0; k < edges.Length; k++)
+                {
+                    if (k == edges.Length - 1)
+                    {
+                        edges[k] = new Edge(points[k], points[0]);
+                    }
+                    else
+                    {
+                        edges[k] = new Edge(points[k], points[k + 1]);
+                    }
+                }
+                triangles[i] = new Triangle(points, edges);
+                Console.WriteLine("Triangele {0}", i + 1);
+                for (int b = 0; b < points.Length; b++)
+                {
+                    Console.WriteLine(points[b].X + " " + points[b].Y);
+                }
+                double perimeter = triangles[i].Perimeter();
+                double area = triangles[i].Area();
+                if (triangles[i].Right())
+                {
+                    sumOfRight += area;
+                }
+                if (triangles[i].Isosceles())
+                {
+                    sumOfIsosceles += area;
+                }
+                Console.WriteLine();
+            }
+            double avgSumOfPerimeters = sumOfRight / count;
+            double avgSumOfArea = sumOfIsosceles / count;
+            Console.WriteLine("Avg sum of right triangle's perimeters = {0}", avgSumOfPerimeters);
+            Console.WriteLine("Avg sum of isoscele triangle's area = {0}", avgSumOfArea);
+            Console.ReadLine();
+        }
     }
 }
